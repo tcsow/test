ce44e46585d1e51a87dc2881e6e77cffa5e08f73
tcsow <tzuchi0822@gmail.com>
2024-08-10 15:40:12 +0800

    init

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        boolean t= false;
+        if(t==true){
+
+        }
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
